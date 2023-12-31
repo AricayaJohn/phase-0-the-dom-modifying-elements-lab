a94b188a601a868a89758af457044e277c512107
John Aricaya <jaricaya@gmail.com>
2023-12-31 15:36:24 -0800

    modifying DOM lab completed

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+//create a dom and remove
+document.querySelector("main#main").remove();
+
+//create a new header that has <h1> node
+const newHeader = document.createElement("h1");
+
+//create in for newHeader
+newHeader.id = "victory"
+
+//creating <h1> node to have "Your-Name is the Champion"
+
+newHeader.textContent = "John is the champion"
